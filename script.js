079b85dbc7b9b8cec1d3711d88314709be0d40e7
Abu <sufianmondal.eee@gmail.com>
2024-01-03 00:51:02 +0600

    key Event

---
+++ script.js
@@ -0,0 +1,37 @@
+//finding the element
+
+// const form = document.querySelector("form");
+// const name = document.querySelector(".form_info #name");
+// const email = document.querySelector(".form_info #email");
+// const password = document.querySelector(".form_info #password");
+
+// form.addEventListener("submit", formHandler);
+
+// function formHandler(e) {
+//   e.preventDefault();
+
+//   const userInfo = {
+//     name: name.value,
+//     email: email.value,
+//     password: password.value,
+//   };
+
+//   // userInfo.name = name.value;
+//   // userInfo.email = email.value;
+//   // userInfo.password = password.value;
+
+//   console.log(userInfo);
+// }
+
+//end of finding the element
+
+// const textarea = document.querySelector("textarea");
+// // textarea.addEventListener("keydown", function () {
+// //   console.log("keydown");
+// // });
+// // textarea.addEventListener("keypress", function () {
+// //   console.log("keypress");
+// // });
+// textarea.addEventListener("keyup", function (e) {
+//   console.log("e.shiftKey");
+// });
