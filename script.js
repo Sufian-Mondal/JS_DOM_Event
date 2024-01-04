650335155873a9080e2c2c5e3cf76c2be3312e2e
Abu <sufianmondal.eee@gmail.com>
2024-01-04 16:47:14 +0600

    dragevent

--- script.js
+++ script.js
@@ -35,3 +35,20 @@
 // textarea.addEventListener("keyup", function (e) {
 //   console.log("e.shiftKey");
 // });
+
+const div = document.querySelector("div");
+const p = document.querySelector("p");
+
+p.addEventListener("dragstart", function (e) {
+  e.dataTransfer.setData("Text", e.target.id);
+});
+
+div.addEventListener("dragover", function (e) {
+  e.preventDefault();
+});
+
+div.addEventListener("drop", function (e) {
+  let id = e.dataTransfer.getData("Text");
+  // console.log(id);
+  div.appendChild(document.getElementById(id));
+});
